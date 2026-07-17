e84e2928951a75237cbc03b6b36ae50a378dbacd
Vjacheslav Melnikov <vjacheslav.melnikov@winfinity.live>
2026-07-17 23:53:47 +0200

    Apply service config during VPS deploy

--- deploy/pull_and_restart.sh
+++ deploy/pull_and_restart.sh
@@ -11,6 +11,12 @@ git reset --hard origin/main
 npm install
 npm run build
 
+install -m 0644 deploy/qualityopsstudio.service /etc/systemd/system/qualityopsstudio.service
+install -m 0644 deploy/nginx/qualityopsstudio.com.conf /etc/nginx/sites-available/qualityopsstudio.com
+ln -sfn /etc/nginx/sites-available/qualityopsstudio.com /etc/nginx/sites-enabled/qualityopsstudio.com
+systemctl daemon-reload
+nginx -t
 chown -R qualityops:qualityops "$APP_DIR"
 sudo systemctl restart qualityopsstudio.service
 sudo systemctl --no-pager --full status qualityopsstudio.service
+sudo systemctl reload nginx
